10b58a5570d67964157bfd1460af283a9ce58ef4
junokreisler <77649114+junokreisler@users.noreply.github.com>
2022-07-07 14:46:15 +0200

    Create combine_SNP_Table.R
    run this to obtain the full SNP table for GAPIT

---
+++ code/combine_SNP_Table.R
@@ -0,0 +1,27 @@
+acc_names <- t(read.csv('acc_excerpt.csv')) 
+files <- list.files(path="/srv/kenlab/ejansone/SNP_Table",
+                    pattern="*.csv.gz", full.names=TRUE, recursive=FALSE)
+cat(length(files), 'files found')
+
+print('Opening initial table...')
+
+SNP_table <- read.table('SNP_table_init.csv.gz', header = FALSE, sep= ',')
+SNP_table <- cbind(acc_names, SNP_table)
+cat('Initial table with dimensions', dim(SNP_table), 'opened, saving as RDS...')
+saveRDS(SNP_table, 'SNP_Table_init.rds')
+print('Initial SNP table saved as SNP_Table_init.rds')
+
+SNP_table <- readRDS('SNP_Table_init.rds')
+
+for (file in files) {
+  cat('Now adding', file, '\n')
+  temp <- read.table(file, header=FALSE, sep= ',') # load file
+  cat('adding dimensions', dim(temp), 'to the SNP table..')
+  SNP_table <- cbind(SNP_table, temp)
+  cat('current table dimensions', dim(SNP_table))
+}
+
+print('Saving SNP table into as SNP_Table_full_AraGWAS.rds ...')
+saveRDS(SNP_table, 'SNP_Table_full_AraGWAS.rds')
+
+print('Table saved successfully.')
